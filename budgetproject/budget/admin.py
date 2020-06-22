b181dda2e99473f85f35ea8ec08e096dedbc042e
Muhammad Abu Bakar <refocuse313@gmail.com>
2020-06-23 02:13:41 +0500

    showing details of budget, expenses  and transections detail on template

--- budgetproject/budget/admin.py
+++ budgetproject/budget/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project,Expenses,Category
 
-# Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Expenses)
+admin.site.register(Category)
